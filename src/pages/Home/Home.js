e61eb0342cfba2894cef28466f08f96e1dd1e011
Minori Hashimoto <minori.fh@gmail.com>
2020-01-29 14:26:10 -0600

    create Home component

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+import HomePage from '../../components/HomePage'
+
+class Home extends Component {
+    render(){
+        return(
+            <HomePage>
+                <h1>Hello Deb</h1>
+                <h2>Links</h2>
+                <h2>MI</h2> 
+                <h2>NoMatch</h2>
+            </HomePage>
+        )
+    }
+}
+
+export default Home;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'; //allows us to utilize JSX 
 import { Route, Switch } from "react-router-dom"; //react-router-dom is a routing lib, we are using the Route and Switch components to tell the application which component to display for which routes
 
+
+//importing components to be used
+import Home from '../pages/Home/Home';
+
 //react is made up of components which can be defined using 1.) functions 2.) classes 
 //1. functional components - simpler, less code, not stateful
 //2. class components - longer, more code, but we can use states! 
