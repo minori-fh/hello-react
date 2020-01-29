86f7609f61babd3affddd20162e1279cdee80c81
Minori Hashimoto <minori.fh@gmail.com>
2020-01-29 11:07:35 -0600

    Updates

---
+++ precache-manifest.d37101c57c1a83a9f4a96d76a499b64c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439803041d7121d2a1d34be9da564bca",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "f18e775583cc1eec75bd",
+    "url": "/hello-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "00a164b505c1734aa846",
+    "url": "/hello-react/static/js/2.368a41ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hello-react/static/js/2.368a41ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f18e775583cc1eec75bd",
+    "url": "/hello-react/static/js/main.31d0ab3b.chunk.js"
+  },
+  {
+    "revision": "f0d834b638037e9fea77",
+    "url": "/hello-react/static/js/runtime-main.a37268f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
