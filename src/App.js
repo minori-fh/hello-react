31fcddb88a866de073534a0df84eb0fe119e59ba
Minori Hashimoto <minori.fh@gmail.com>
2020-01-29 14:53:11 -0600

    edit App component to incorporate Routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// import routes
+import Routes from './routes/Routes';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      home: true,
+      links: false,
+      MI: false,
+      CTAs: false,
+    }
+  }
+
+  ONRouting = () => {
+    return(
+      <Router>
+        <div>
+          <Switch>
+            <Route path="*" component={Routes} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+
+  render(){
+    switch(this.state.home){
+      case true:
+        return this.ONRouting()
+      default:
+        return this.ONRouting()
+    }
+  }
 }
 
 export default App;
