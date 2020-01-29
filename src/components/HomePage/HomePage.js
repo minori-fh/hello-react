de64c5e7d3a568b34ad908f1ff6d468983e2db55
Minori Hashimoto <minori.fh@gmail.com>
2020-01-29 14:32:43 -0600

    create HomePage component

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = (props) => {
+    <div>
+        <div>
+            <h1>Hello</h1>
+        </div>
+    </div>
+}
+
+export default HomePage; 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-import HomePage from '../../components/HomePage'
+import HomePage from '../../components/HomePage/HomePage'
 
 class Home extends Component {
     render(){
