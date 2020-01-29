5abcdd74b6970cac6ced7f839cbe6e6a25710aa0
Minori Hashimoto <minori.fh@gmail.com>
2020-01-29 15:58:11 -0600

    fix parenthesis syntax in homepage component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const HomePage = (props) => {
+const HomePage = (props) => (
     <div>
         <div>
             {props.children}
         </div>
     </div>
-}
+);
 
 export default HomePage; 
