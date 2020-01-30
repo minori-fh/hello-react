13d21cfd8195eb4546e0a47ff8c8ee5ef34745c5
Minori Hashimoto <minori.fh@gmail.com>
2020-01-29 20:28:30 -0600

    remove unnecessary files

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'; //allows us to utilize JSX 
 import { Route, Switch } from "react-router-dom"; //react-router-dom is a routing lib, we are using the Route and Switch components to tell the application which component to display for which routes
 
-
 //importing components to be used
 import Home from '../pages/Home/Home';
-import Links from '../pages/Links/Links';
 
 //react is made up of components which can be defined using 1.) functions 2.) classes 
 //1. functional components - simpler, less code, not stateful
@@ -16,10 +14,6 @@ class Routes extends Component {
         return(
             <Switch>
                 <Route exact path="/" component={Home}/>
-                <Route exact path="/links" component={Links}/>
-                {/* <Route exact path="/MI" component={MI}/>
-                <Route exact path="/CTA" component={CTAs}/>
-                <Route exact path="*" component={NoMatch}/> I like to have a * catch-all in case someone puts in a random path */}
             </Switch>
         )
     }
